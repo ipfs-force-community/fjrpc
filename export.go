d449852382407aebcb97166c2afefd3332686e82
dtynn <dtynn@163.com>
2021-04-20 11:42:24 +0800

    yet another jsonrpc impl

---
+++ export.go
@@ -0,0 +1,20 @@
+package jsonrpc
+
+import (
+	gj "github.com/filecoin-project/go-jsonrpc"
+)
+
+// export things from go-jsonrpc
+type ClientCloser = gj.ClientCloser
+type Config = gj.Config
+type Option = gj.Option
+type ServerOption = gj.ServerOption
+
+var (
+	WithReconnectBackoff = gj.WithNoReconnect
+	WithPingInterval     = gj.WithPingInterval
+	WithTimeout          = gj.WithTimeout
+	WithNoReconnect      = gj.WithNoReconnect
+	WithParamEncoder     = gj.WithParamEncoder
+	WithMaxRequestSize   = gj.WithMaxRequestSize
+)
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ipfs-force-community/fjrpc
+
+go 1.15
+
+require (
+	github.com/filecoin-project/go-jsonrpc v0.1.3
+	github.com/ipfs/go-log/v2 v2.1.3
+	go.opencensus.io v0.23.0
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+)
